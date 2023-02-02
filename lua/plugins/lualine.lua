fd1d65fd3034e8cf5274df1065ea2b6f7ad8fa43
Shad Kaske <shadkaske@gmail.com>
2023-02-02 13:07:43 -0600

    LuaLine Fairly Sorted

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,137 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  opts = function(plugin)
+    local icons = require("lazyvim.config").icons
+
+    local function fg(name)
+      return function()
+        ---@type {foreground?:number}?
+        local hl = vim.api.nvim_get_hl_by_name(name, true)
+        return hl and hl.foreground and { fg = string.format("#%06x", hl.foreground) }
+      end
+    end
+
+    --- @param trunc_width number trunctates component when screen width is less then trunc_width
+    --- @param trunc_len number truncates component to trunc_len number of chars
+    --- @param hide_width number hides component when window width is smaller then hide_width
+    --- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
+    --- return function that can format the component accordingly
+    local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+      return function(str)
+        local win_width = vim.fn.winwidth(0)
+        if hide_width and win_width < hide_width then
+          return ""
+        elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+          return str:sub(1, trunc_len) .. (no_ellipsis and "" or "...")
+        end
+        return str
+      end
+    end
+
+    local custom_fname = require("lualine.components.filename"):extend()
+    local highlight = require("lualine.highlight")
+    local default_status_colors = { saved = "#98be65", modified = "#ec5f67" }
+
+    function custom_fname:init(options)
+      custom_fname.super.init(self, options)
+      self.status_colors = {
+        saved = highlight.create_component_highlight_group(
+          { fg = default_status_colors.saved },
+          "filename_status_saved",
+          self.options
+        ),
+        modified = highlight.create_component_highlight_group(
+          { fg = default_status_colors.modified },
+          "filename_status_modified",
+          self.options
+        ),
+      }
+      if self.options.color == nil then
+        self.options.color = ""
+      end
+    end
+
+    function custom_fname:update_status()
+      local data = custom_fname.super.update_status(self)
+      data = highlight.component_format_highlight(
+        vim.bo.modified and self.status_colors.modified or self.status_colors.saved
+      ) .. data
+      return data
+    end
+
+    local function diff_source()
+      local gitsigns = vim.b.gitsigns_status_dict
+      if gitsigns then
+        return {
+          added = gitsigns.added,
+          modified = gitsigns.changed,
+          removed = gitsigns.removed,
+        }
+      end
+    end
+
+    return {
+      options = {
+        theme = "auto",
+        globalstatus = false,
+        disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
+      },
+      sections = {
+        lualine_a = {
+          {
+            "mode",
+            fmt = function(str)
+              return str:sub(1, 1)
+            end,
+          },
+        },
+        lualine_b = {
+          {
+            "b:gitsigns_head",
+            icon = "",
+            color = { gui = "bold" },
+          },
+        },
+        lualine_c = {
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { custom_fname, fmt = trunc(90, 30, 50, false), path = 1 },
+        },
+        lualine_x = {
+          -- stylua: ignore
+          { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = fg("Special") },
+          {
+            "diff",
+            source = diff_source,
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+            padding = { left = 2, right = 1 },
+            diff_color = {
+              added = { fg = "#98be65" },
+              modified = { fg = "#ECBE7B" },
+              removed = { fg = "#ec5f67" },
+            },
+          },
+        },
+        lualine_y = {
+          { "progress", separator = "", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {},
+      },
+      extensions = { "neo-tree" },
+    }
+  end,
+}
