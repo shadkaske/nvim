b9e58c01afd9ada1639174420ee592b6931f4ead
Shad Kaske <shadkaske@gmail.com>
2023-02-03 08:38:23 -0600

    Keymaps and lazy loading for nvim-dap

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -1,22 +1,5 @@
 return {
   "rcarriga/nvim-dap-ui",
   dependencies = { "mfussenegger/nvim-dap" },
-  config = function()
-    require("dapui").setup()
-    local status_ok, dap = pcall(require, "dap")
-    if not status_ok then
-      return
-    end
-    local dapui = require("dapui")
-
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated["dapui_config"] = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited["dapui_config"] = function()
-      dapui.close()
-    end
-  end,
+  lazy = true,
 }
