77cdbfbe51750def3037d52f976a7d685e3bdf00
Shad Kaske <shadkaske@gmail.com>
2023-02-03 08:17:47 -0600

    Fugitive Installed

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,20 @@
+return {
+  "tpope/vim-fugitive",
+  lazy = true,
+  cmd = {
+    "G",
+    "Git",
+    "Gdiffsplit",
+    "Gread",
+    "Gwrite",
+    "Ggrep",
+    "GMove",
+    "GDelete",
+    "GBrowse",
+    "GRemove",
+    "GRename",
+    "Glgrep",
+    "Gedit",
+  },
+  ft = { "fugitive" },
+}
