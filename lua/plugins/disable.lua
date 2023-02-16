39cc8cea53770ced68b6a6aaeedf6aac929eb062
Shad Kaske <shadkaske@gmail.com>
2023-02-15 18:14:39 -0600

    Removed some previuosly disabled plugins

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,8 +1,4 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
-  { "SmiteshP/nvim-navic", enabled = false },
-  { "windwp/nvim-spectre", enabled = false },
   { "RRethy/vim-illuminate", enabled = false },
-  -- { "RRethy/vim-illuminate", enabled = false },
 }
