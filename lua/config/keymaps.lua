aef8734f603fe2925dd8c33d032c82595a23f4b2
Shad Kaske <shadkaske@gmail.com>
2025-02-13 13:42:21 -0600

    Added Custom Keymaps and removed the leader-l for lazy

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,31 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Delete some built in keymaps
+vim.keymap.del("n", "<leader>l")
+
+-- Move Lines
+vim.keymap.set("n", "<A-S-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+vim.keymap.set("n", "<A-S-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+vim.keymap.set("i", "<A-S-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+vim.keymap.set("i", "<A-S-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+vim.keymap.set("v", "<A-S-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+vim.keymap.set("v", "<A-S-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+
+-- Line Ending Shortcuts
+vim.keymap.set("i", "<A-e>", "<C-o>$", { silent = true })
+vim.keymap.set("i", "<A-;>", "<C-o>A;<ESC>", { silent = true })
+vim.keymap.set("i", "<A-,>", "<C-o>A,<ESC>", { silent = true })
+vim.keymap.set("n", "<A-;>", "A;<ESC>", { silent = true })
+vim.keymap.set("n", "<A-,>", "A,<ESC>", { silent = true })
+
+-- Center view on jumps
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
+vim.keymap.set("n", "<C-o>", "<C-o>zz")
+vim.keymap.set("n", "<C-i>", "<C-i>zz")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
