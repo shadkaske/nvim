9ef7d58fdcaa28ee7e8898f958eff3a15f21dab6
Shad Kaske <shadkaske@gmail.com>
2025-02-13 14:13:02 -0600

    SmartSplits installed

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,28 @@
 -- Delete some built in keymaps
 vim.keymap.del("n", "<leader>l")
 
+-- Smart Splits
+-- resizing splits
+vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
+vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
+vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
+vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
+-- moving between splits
+vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+-- swapping buffers between windows
+vim.keymap.set("n", "<leader>bsh", require("smart-splits").swap_buf_left, { desc = "Swap Buffer Left" })
+vim.keymap.set("n", "<leader>bsj", require("smart-splits").swap_buf_down, { desc = "Swap Buffer Down" })
+vim.keymap.set("n", "<leader>bsk", require("smart-splits").swap_buf_up, { desc = "Swap Buffer Up" })
+vim.keymap.set("n", "<leader>bsl", require("smart-splits").swap_buf_right, { desc = "Swap Buffer Right" })
+-- terminal mode
+vim.keymap.set("t", "<C-h>", require("smart-splits").move_cursor_left)
+vim.keymap.set("t", "<C-j>", require("smart-splits").move_cursor_down)
+vim.keymap.set("t", "<C-k>", require("smart-splits").move_cursor_up)
+vim.keymap.set("t", "<C-l>", require("smart-splits").move_cursor_right)
+
 -- Move Lines
 vim.keymap.set("n", "<A-S-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
 vim.keymap.set("n", "<A-S-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
