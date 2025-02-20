57c9c1979b37ebfe7d2e6673d887931e94cf34d3
Shad Kaske <shadkaske@gmail.com>
2025-02-20 17:15:19 -0600

    Added btxml file type

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,10 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Set Btxml Files to xml filetype
+vim.api.nvim_create_autocmd(
+  { "BufWritePre", "BufNewFile" },
+  { pattern = { "*.btxml" }, command = [[set filetype=xml]] }
+)
+
