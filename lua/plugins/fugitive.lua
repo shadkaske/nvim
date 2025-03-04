fc39c64033cd74dc38efd7573589c4a0a4994fef
Shad Kaske <shadkaske@gmail.com>
2025-03-04 12:17:37 -0600

    Added fugitive, becuase I like it

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+      "G",
+      "GMove",
+      "GRemove",
+      "GRename",
+      "Gdiffsplit",
+      "Gedit",
+      "Ggrep",
+      "Git",
+      "Glgrep",
+      "Gread",
+      "Gvdiffsplit",
+      "Gwrite",
+    },
+  },
+}
