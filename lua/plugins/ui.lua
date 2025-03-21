f9acb95ddd214e9ad883772f0d79705a8ccc125d
Shad Kaske <shadkaske@gmail.com>
2025-03-21 10:28:52 -0500

    LuaLine format update

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,7 +3,7 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = {
       options = {
-        globalstatus = false,
+        globalstatus = true,
         component_separators = { left = "", right = "" },
         section_separators = { left = " ", right = " " },
         disabled_filetypes = {
