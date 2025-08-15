5e83e58740d0fd9e454ad8e98a3697e5db7299c6
Shad Kaske <shadkaske@gmail.com>
2025-08-15 10:31:07 -0500

    Added oil git status and q keymap to quit oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,14 +1,19 @@
 return {
   'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    win_options = {
+      signcolumn = 'yes:2',
+    },
+    keymaps = {
+      ['q'] = { 'actions.close', mode = 'n' },
+    },
+  },
   dependencies = {
     { 'nvim-tree/nvim-web-devicons', opts = {} },
   },
   lazy = false,
   keys = {
-    { '-', '<cmd>Oil<CR>', mode = 'n', desc = 'Open Oil' },
-    -- { '<leader>e', '<cmd>lua require("oil").toggle_float()<CR>', mode = 'n', desc = 'Toggle Oil Float' },
+    -- { '-', '<cmd>Oil<CR>', mode = 'n', desc = 'Open Oil' },
+    { '-', '<cmd>lua require("oil").toggle_float()<CR>', mode = 'n', desc = 'Toggle Oil Float' },
   },
 }
