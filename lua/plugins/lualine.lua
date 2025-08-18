7cf55d73e54ea8b582e51da99e5d53b02b66ee93
Shad Kaske <shadkaske@gmail.com>
2025-08-18 09:44:41 -0500

    Renamed ui.lua to lualine.lua

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,44 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    options = {
+      globalstatus = false,
+      component_separators = { left = "", right = "" },
+      section_separators = { left = " ", right = " " },
+      disabled_filetypes = {
+        statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard", "dbui" },
+      },
+    },
+    inactive_sections = {
+      lualine_c = {
+        {
+          "filename",
+          path = 1,
+        },
+      },
+      lualine_x = { "location" },
+    },
+    sections = {
+      lualine_a = {
+        {
+          "mode",
+          fmt = function(str)
+            return str:sub(1, 1)
+          end,
+        },
+      },
+      lualine_b = {
+        {
+          "branch",
+          icon = "",
+        },
+      },
+      lualine_y = {
+        { "progress" },
+      },
+      lualine_z = {
+        { "location" },
+      },
+    },
+  },
+}
