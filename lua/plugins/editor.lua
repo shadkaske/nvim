7bb34350551bc578cd55f0ed9d4df949b122e5a3
Shad Kaske <shadkaske@gmail.com>
2025-08-18 09:45:45 -0500

    Streamlining which key overrides

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,14 +1,12 @@
 return {
-  {
-    "folke/which-key.nvim",
-    opts = {
-      preset = "classic",
-    },
-    init = function()
-      require("which-key").add({
-        { "<leader>l", group = "Laravel", icon = { icon = "", color = "red" } },
-        { "<leader>t", group = "Ansible", icon = { icon = "", color = "blue" } },
-      })
-    end,
+  "folke/which-key.nvim",
+  opts = {
+    preset = "modern",
   },
+  init = function()
+    require("which-key").add({
+      { "<leader>l", group = "Laravel", icon = { icon = "", color = "red" } },
+      { "<leader>t", group = "Ansible", icon = { icon = "", color = "blue" } },
+    })
+  end,
 }
