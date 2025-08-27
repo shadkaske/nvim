3eb047cf8b8f9f9c93a5183bff01235168469850
Shad Kaske <shadkaske@gmail.com>
2025-08-27 15:42:52 -0500

    Decided I do want betterquickfix

--- lua/plugins/betterquickfix.lua
+++ lua/plugins/betterquickfix.lua
@@ -1,5 +1,5 @@
 return {
-  -- "kevinhwang91/nvim-bqf",
-  -- event = { "BufReadPost", "BufNewFile" },
-  -- opts = {},
+  "kevinhwang91/nvim-bqf",
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {},
 }
