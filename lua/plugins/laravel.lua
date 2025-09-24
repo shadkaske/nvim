93ec7d6f007cd6a2793f00c117925ace6409c9b9
Shad Kaske <shadkaske@gmail.com>
2025-09-24 14:20:51 -0500

    Removed no redundant blade treesitter config

--- lua/plugins/laravel.lua
+++ lua/plugins/laravel.lua
@@ -47,32 +47,7 @@ return {
     },
   },
   {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "blade", "php_only" })
-    end,
-    config = function(_, opts)
-      vim.filetype.add({
-        pattern = {
-          [".*%.blade%.php"] = "blade",
-        },
-      })
-
-      require("nvim-treesitter.configs").setup(opts)
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      ---@diagnostic disable-next-line
-      parser_config.blade = {
-        install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "blade",
-      }
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "blade-formatter",
