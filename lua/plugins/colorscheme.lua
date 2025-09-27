1d8891b3672a11b2b58e21ac79b57b5d9471dc77
Shad Kaske <shadkaske@gmail.com>
2025-09-27 08:04:47 -0500

    Removed Catppuccin Overrides for latest version of LazyVim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,16 +13,6 @@ return {
   },
   {
     "catppuccin/nvim",
-    opts = function(_, opts)
-      local module = require("catppuccin.groups.integrations.bufferline")
-      if module then
-        module.get = module.get_theme
-      end
-
-      opts.transparent_background = true
-
-      return opts
-    end,
   },
   {
     "LazyVim/LazyVim",
